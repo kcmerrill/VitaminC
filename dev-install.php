920b97b46c27cd040218409aae119db3a407a4b8
kc merrill <kcmerrill@gmail.com>
2013-08-26 22:50:51 -0600

    Updates to the dev-install script

--- dev-install.php
+++ dev-install.php
@@ -1,4 +1,6 @@
 <?php
 $kiss = file_get_contents('https://github.com/kcmerrill/kiss/archive/master.zip');
 file_put_contents('kiss.zip', $kiss);
-`unzip kiss.zip -d .;cp -rf kiss-master/* .;rm -rf kiss.zip;`;
+`unzip kiss.zip -d .;cp -rf kiss-master/* .;rm -rf kiss.zip;rm -rf kiss-master;`;
+`php -r "eval('?>'.file_get_contents('https://getcomposer.org/installer'));"`;
+`php composar.phar install`;
