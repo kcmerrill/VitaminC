d7e2ef74c590fdac3981b5c14b11032d4f052f48
kc merrill <kcmerrill@gmail.com>
2013-08-26 22:58:22 -0600

    Woot! I think we have a simple dev install script
    Script does the following:
    - Downloads KISS microframework
    - Unzips the contents, and shuffles it about in the local directory
    - Downloads composer
    - Runs install on the composer.json file for KISS.
    
    Voila! Done!

--- dev-install.php
+++ dev-install.php
@@ -3,4 +3,4 @@ $kiss = file_get_contents('https://github.com/kcmerrill/kiss/archive/master.zip'
 file_put_contents('kiss.zip', $kiss);
 `unzip kiss.zip -d .;cp -rf kiss-master/* .;rm -rf kiss.zip;rm -rf kiss-master;`;
 `php -r "eval('?>'.file_get_contents('https://getcomposer.org/installer'));"`;
-`php composar.phar install`;
+`./composer.phar install`;
